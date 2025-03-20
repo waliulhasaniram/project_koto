aad1193d3309c8cd0fb2056c5de46818abb85ee8
waliulhasaniram <waliulhasaniram000@gmail.com>
2025-03-20 23:20:59 +0600

    koto commit

--- backend/Product/multerMiddleware.js
+++ backend/Product/multerMiddleware.js
@@ -2,7 +2,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './public/temp')
+        cb(null, './public')
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
