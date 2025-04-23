171316823a8262ee7ecb89a26b9f27db0432a339
Md. Waliul Hasan Iram <97869370+waliulhasaniram@users.noreply.github.com>
2025-04-23 20:36:48 +0600

    Update Product.route.js

--- backend/Product/Product.route.js
+++ backend/Product/Product.route.js
@@ -19,4 +19,10 @@ router.route("/searched-product").get(productController.GetSearchProduct)
 
 router.route("/delece-product/:id").delete(productController.deleteProduct)
 
+
+router.route("/a_z").get(productController.GetAllProduct_A_Z)
+router.route("/z_a").get(productController.GetAllProduct_Z_A)
+router.route("/low_high").get(productController.GetAllProduct_LOW_HIGH)
+router.route("/high_low").get(productController.GetAllProduct_HIGH_LOW)
+
 module.exports = router;
